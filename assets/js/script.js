31f3b7c3472c1c5d6a4ec5eb04bc7a9bea85feaa
CrazyVideoGamez <jasonanyilian@gmail.com>
2019-12-23 16:05:36 -0500

    Fixed bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,3 @@
-if (window.navigator.userAgent.indexOf("Edge") > -1) {
-    let division = document.getElementById('division-background');
-    division.id = '';
-    
-    let svg = document.createElement('svg');
-    svg.width = '0'
-    svg.height = '0'
-
-    let clipPath = document.createElement('clipPath');
-    clipPath.id = "myclippath"
-    clipPath.clipPathUnits = 'objectBoundingBox'
-
-    let polygon = doucment.createElement('polygon');
-    polygon.points = '1, 0.29, 1, 1, 0, 1, 0, 0.50'
-
-    let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '../css/clippath.css'
-
-
-    let edge = document.getElementById('edge');
-    edge.appendChild(link);
-    edge.appendChild(svg);
-
-    svg.appendChild(clipPath);
-    clipPath.appendChild(polygon);
-}
-
 function github(){
     window.open('https://www.github.com/crazyvideogamez', target='_top');
 }
