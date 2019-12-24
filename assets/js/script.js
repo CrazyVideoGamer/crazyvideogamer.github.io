06265ae68ddddfe4049fc47940494333b5a016b6
CrazyVideoGamez <jasonanyilian@gmail.com>
2019-12-24 13:23:58 -0500

    Made the navbar disappear when scrolling down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 function github(){
     window.open('https://www.github.com/crazyvideogamez', target='_top');
 }
+
+let prevScrollpos = window.pageYOffset, currentScrollPos = undefined;
+window.onscroll = function() {
+    currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementsByTagName('nav')[0].style.top = '0';
+    } else {
+        document.getElementsByTagName('nav')[0].style.top = "-30px";
+    }
+    prevScrollpos = currentScrollPos;
+}
