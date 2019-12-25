ace4bd6c3b4b2a3935d6ecb38afd07f2e18ec1dc
CrazyVideoGamez <crazyvideogamez.help@gmail.com>
2019-12-25 12:21:24 -0500

    Added github octoarm

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ function github(){
     window.open('https://www.github.com/crazyvideogamez', target='_top');
 }
 
+height = $(document).height();
+
 let prevScrollpos = window.pageYOffset, currentScrollPos = undefined;
 window.onscroll = function() {
     currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementsByTagName('nav')[0].style.top = '0px';
+        document.getElementsByClassName('github-corner')[0].style.top = '0px';
     } else {
-        if (currentScrollPos > 35) {
-            document.getElementsByTagName('nav')[0].style.top = "-64px";
+        if (currentScrollPos > 35 || height === 783) {
+            document.getElementsByTagName('nav')[0].style.top = "-84px";
+            document.getElementsByClassName('github-corner')[0].style.top = '-84px';
         }
     }
     prevScrollpos = currentScrollPos;
